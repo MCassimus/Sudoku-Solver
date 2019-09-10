b450aff65949b762738f9814f6b1bab61708b3d7
Michael Cassimus <16908265+MCassimus@users.noreply.github.com>
2019-09-10 15:25:09 -0500

    add some comments and do a little clean up

--- Source/Main.java
+++ Source/Main.java
@@ -1,19 +1,37 @@
+/*
+ * File : Main.java
+ * Date : 9/10/2019
+ * Authors : Michael Cassimus + Tim Peters
+ * Project URL : https://github.com/MCassimus/Sudoku-Solver/
+ * Description : This is the entry point of the application, and is the "controller" of the 
+ * 				 program's flow as it calls methods from other classes to complete its task
+ */
+
 import java.util.Scanner; //input
 
-public class Main{
-	public static String fileName = "../Puzzles/";
+
+public class Main
+{
+	//Class members
+	public static String fileBase = "../Puzzles/";
 	
-	public static void main(String [] args){
-		// create Scanner to obtain input from command window
-      	Scanner input = new Scanner(System.in);
+	
+	//Entry point of the application
+	public static void main(String [] args)
+	{
+		//Method variables are declared here
+		Scanner input = new Scanner(System.in);
+      	String selectedPuzzleFile = "";
       	
+      	//Gather input from the user on which puzzle they want to select
       	System.out.print("Enter Sudoku Puzzle File Name : ");
-      	fileName += input.nextLine();
+      	selectedPuzzleFile += input.nextLine();
       	
-      	PuzzleSolver solver = new PuzzleSolver("");
+      	//Initialize the puzzle solver and start the solving algorithm
+      	PuzzleSolver solver = new PuzzleSolver(fileBase + selectedPuzzleFile);
       	solver.printPuzzle();
       	
-      	
+      	//Closing the application
       	System.out.print("Press any key to continue ");
       	input.next();
 	}
--- Source/PuzzleSolver.java
+++ Source/PuzzleSolver.java
@@ -1,9 +1,23 @@
+/*
+ * File : PuzzleSolver.java
+ * Date : 9/10/2019
+ * Authors : Michael Cassimus + Tim Peters
+ * Project URL : https://github.com/MCassimus/Sudoku-Solver/
+ * Description : This class holds the main logic of the application and will hold methods
+ * 				 such as printing, importing and exporting, and solving the puzzle.
+ */
+
+
 public class PuzzleSolver{
-	public int[][] puzzle = new int[9][9];
+	//Class members
+	private int[][] puzzle = new int[9][9];
 	
+	
+	//Constructor
 	public PuzzleSolver(String fileName)
 	{
 		//initialize the puzzle
+		//this loop is temporary
 		for(int y = 0; y < 9; y++)
 		{
 			for(int x = 0; x < 9; x++)
@@ -13,27 +27,37 @@ public class PuzzleSolver{
 		}
 	}
 	
+	
+	//Prints the puzzle stored in the puzzle array variable
 	public void printPuzzle()
 	{
 		for(int y = 0; y < 9; y++)
 		{
+			//we are at the top of a cell
 			if(y % 3 == 0)
 			{
+				//print a border
 				System.out.println("*---*---*---*");	
 			}
 			
 			for(int x = 0; x < 9; x++)
 			{
+				//we are at the edge of a cell
 				if(x % 3 == 0)
 				{
+					//print a border
 					System.out.print("|");	
 				}
 				
+				//show the actual numbers of the puzzle at the current location
 				System.out.print(puzzle[x][y]);
 			}
 			
-			System.out.println("|");//add line break and final character
+			//add line break and final character
+			System.out.println("|");
 		}
-		System.out.println("*---*---*---*");//cap off the bottom of the puzzle	
+		
+		//cap off the bottom of the puzzle
+		System.out.println("*---*---*---*");	
 	}
 }
